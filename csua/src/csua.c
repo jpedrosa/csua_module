2e984ba6d3fb5846b1573fe518fc4708c9c0cf27
Joao Pedrosa <joaopedrosa@gmail.com>
2015-12-25 09:49:52 -0300

    Remove the support for the open C function because Swift comes support for it now.

--- csua/src/csua.c
+++ csua/src/csua.c
@@ -1,10 +1,3 @@
 #include "csua.h"
 
-// Swift has a limitation when mapping to C functions that have variadic
-// parameters. This function sets the fixed parameters to provide the hints
-// that Swift needs.
-extern int csua_open(const char *path, int oflag, mode_t mode) {
-  return open(path, oflag, mode);
-}
-
 extern char **environ;
--- csua/src/csua.h
+++ csua/src/csua.h
@@ -1,6 +1,4 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-extern int csua_open(const char *path, int oflag, mode_t mode);
-
 extern char **environ;
